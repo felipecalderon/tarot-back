15196a8d0374f3d7038f68c67ba3292057ba92f7
Felipe <felipe.calderon321@gmail.com>
2024-06-06 22:07:25 -0400

    cors config socket

--- src/app.ts
+++ src/app.ts
@@ -5,7 +5,20 @@ import { ioServer } from './io'
 import { env } from './config'
 const app = express()
 const server = http.createServer(app)
-const io = new Server(server)
+const io = new Server(server, {
+    cors: {
+        origin: (origin, callback) => {
+            const allowedOrigins = ['http://localhost:3000', 'https://tarot-back-production.up.railway.app']
+            if (!origin) return callback(null, true) // Permitir clientes sin origen
+            if (allowedOrigins.indexOf(origin) !== -1) {
+                callback(null, true)
+            } else {
+                callback(new Error('Origen no permitido por CORS'))
+            }
+        },
+        methods: ['GET', 'POST'],
+    },
+})
 
 ioServer(io)
 
