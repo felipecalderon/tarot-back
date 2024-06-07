8d1312f0e4e3e3acd314664d11f11f81a5adb11a
Felipe <felipe.calderon321@gmail.com>
2024-06-07 09:41:52 -0400

    socket endpoint to test

--- src/io.ts
+++ src/io.ts
@@ -3,14 +3,17 @@ import { chat } from './chat'
 
 export const ioServer = (io: Server) => {
     io.on('connection', (socket) => {
-        console.log('usuario conectado')
-
         socket.on('data', async (data) => {
-            console.log(data.name)
+            console.log({ usuario: data.name, consulta: data.question })
+
             if (data) {
                 const response = await chat(data)
                 socket.emit('response', response)
             }
+        })
+
+        socket.on('test', () => {
+            socket.emit('response', 'conexión correcta')
         })
 
         socket.on('disconnect', () => {
