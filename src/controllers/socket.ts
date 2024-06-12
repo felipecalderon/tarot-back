4ac93324131817f60d4bf728343a02e8d29b8515
Felipe <felipe.calderon321@gmail.com>
2024-06-11 23:44:28 -0400

    refactor folders/files

--- src/controllers/socket.ts
+++ src/controllers/socket.ts
@@ -1,23 +1,25 @@
 import { Socket } from 'socket.io'
 import { chat } from '../utils/chat'
+import { ReqProps } from '../config/interfaces'
 
-export const ioController = (socket: Socket) => {
-    console.log('usuario conectado')
-
-    socket.on('data', async (data) => {
+const ioChat = async (data: ReqProps, socket: Socket) => {
+    try {
         console.log({ usuario: data.name, consulta: data.question })
 
         if (data) {
             const response = await chat(data)
             socket.emit('response', response)
         }
-    })
+    } catch (error) {
+        console.log({ error })
+        socket.emit('error', 'Ha ocurrido un error')
+    }
+}
 
-    socket.on('test', () => {
-        socket.emit('response', 'conexión correcta')
-    })
+export const ioController = (socket: Socket) => {
+    console.log('usuario conectado')
 
-    socket.on('disconnect', () => {
-        console.log('usuario desconectado')
-    })
+    socket.on('data', async (data: ReqProps) => ioChat(data, socket))
+    socket.on('test', () => socket.emit('response', 'conexión correcta'))
+    socket.on('disconnect', () => console.log('usuario desconectado'))
 }
